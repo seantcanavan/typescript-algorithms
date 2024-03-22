f19580e2ff49b676c7d871c6c3c87da435ff1ab2
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-03-22 13:02:03 -0500

    finished add / delete / find
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- index.ts
+++ index.ts
@@ -7,15 +7,59 @@ class BinaryTree<T> {
     }
 
     add(t: T): Node<T> {
-        return addNode(t)
+        if (this.head === undefined) {
+            this.head = new Node<T>(t)
+        }
+
+        return this.addNode(t, this.head)
     }
 
-    private addNode(t: T) Node<T> {
-
+    delete(t: T): Node<T> | undefined {
+        return this.deleteNode(t, this.head)
     }
 
     find(t: T): Node<T> | undefined {
         return this.findNode(t, this.head)
+    }
+
+    print() {
+        if (this.mode == Mode.InOrder) {
+            this.printInOrder()
+        } else if (this.mode == Mode.BreadthFirstSearch) {
+            this.printBreadthFirst()
+        } else if (this.mode == Mode.DepthFirstSearch) {
+            this.printDepthFirst()
+        }
+    }
+
+
+    private addNode(t: T, head: Node<T> | undefined): Node<T> {
+        if (head === undefined) {
+            return new Node<T>(t)
+        } else if (t > head.data) {
+            return this.addNode(t, head.right)
+        } else if (t < head.data) {
+            return this.addNode(t, head.left)
+        } else {
+            return head
+        }
+    }
+
+    private deleteNode(t: T, head: Node<T> | undefined): Node<T> | undefined {
+        if (head === undefined) {
+            return undefined
+        }
+
+        if (t > head.data) {
+            return this.deleteNode(t, head.right)
+        } else if (t < head.data) {
+            return this.deleteNode(t, head.left)
+        } else {
+            const maxOfMin: Node<T> = this.getMaxOfMin(head)
+            const oldData: T = head.data
+            head.data = maxOfMin.data
+            return this.deleteNode(oldData, head)
+        }
     }
 
     private findNode(t: T, head: Node<T> | undefined): Node<T> | undefined {
@@ -30,6 +74,27 @@ class BinaryTree<T> {
         } else {
             return head
         }
+    }
+
+    private getMaxOfMin(head: Node<T>): Node<T> {
+        let currentHead: Node<T> = head
+        while (currentHead.left !== undefined) {
+            currentHead = currentHead.left
+        }
+
+        return currentHead
+    }
+
+    private printInOrder(): void {
+
+    }
+
+    private printBreadthFirst(): void {
+
+    }
+
+    private printDepthFirst(): void {
+        
     }
 }
 
